e37f7f0c019943bb9eaf4b7cca5d714f74c64d08
Barbara-Mase <mase.barbara@gmail.com>
2026-01-12 16:19:58 +0000

    Jour 5 - fin de journée - mini-projet non fini

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,12 +1,68 @@
 window.addEventListener('DOMContentLoaded', () => {
-
-
-    let box1 = document.getElementById('box-1')
     
-    box1.addEventListener('click', (event) => {
+    let boxes = document.querySelectorAll("div")
+    
+    //ajouter cercle
+    function addCircle (box) {
+        let circle = document.createTextNode('o');
+        box.appendChild(circle);
+    }
+    
+
+    
+    function getFirstEmptyBox(boxes, emptyBox) {
+        for (let box of boxes) {
+            if(box.textContent.trim() === "") {
+                let emptyBox = box;
+            }
+        }
+        return emptyBox;
+     }
+     
+    let affichage = getFirstEmptyBox(boxes); 
+    console.log(affichage)
+
+    function addCross(emptyBox) {
+        let cross = document.createTextNode('x');
+        emptyBox.appendChild(cross);
+
+    }
+       
+    
+    // TEST fonction play
+    function play(boxes, emptyBox) {
+        for (let box of boxes) {
+            box.addEventListener('click', (event) => {
+                addCircle(box);
+                addCross(emptyBox);
+            });
+        }
+    }
         
-        let cross = document.createTextNode('x')
-        box1.appendChild(cross);
-    })
+    play(boxes);
+    
 });
+
+
+
+
+//Récupération de la boite
+// function getBox () {
+//     let boxes = document.querySelectorAll("div:not(:first-of-type)")
+// }
+
+
+
+
+
+// function play (boxes) {
+//     getBox();
+//     boxes.addEventListener('click', (event) => {
+//         for (let box of boxes) {
+//             addCircle();
+//         }
+//     });
+// }
+
+// play()
 
